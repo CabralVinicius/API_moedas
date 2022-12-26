ba702d6f74591cb4beea8a0bdc6c2afe3a1d20a9
Vinicius Cabral <viniciuscabral@MacBook-Air-de-Vinicius.local>
2022-12-26 20:34:58 -0300

    Atividade para treinar e salvar API

---
+++ API_cotacao_dollar.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import time
+import datetime
+
+while True:
+    requisic = requests.get(
+        'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+
+    cotacao = json.loads(requisic.text)
+    print("=== Cotações de moeda ===", datetime.datetime.now())
+    print(cotacao['USDBRL']['name'])
+    print(cotacao['USDBRL']['high'])
+    print(cotacao['USDBRL']['low'])
+    print(cotacao['USDBRL']['create_date'])
+    print("\n")
+    print(cotacao['EURBRL']['name'])
+    print(cotacao['EURBRL']['high'])
+    print(cotacao['EURBRL']['low'])
+    print(cotacao['EURBRL']['create_date'])
+    print("\n")
+    print(cotacao['BTCBRL']['name'])
+    print(cotacao['BTCBRL']['high'])
+    print(cotacao['BTCBRL']['low'])
+    print(cotacao['BTCBRL']['create_date'])
+    time.sleep(5)
+    print("\n")
